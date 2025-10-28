b997c5a1e4c242c3ab1de38e6e253dd87fbd1b4d
Josh Liuaana <jfliuaana@gmail.com>
2025-10-28 16:27:59 +1300

    feat: deployed to firebase hosting

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,18 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import type { UserConfig } from "vitest/node";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    visualizer({
+      open: true,
+      filename: "dist/stats.html",
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
